3251fe4436e5e85b308065ec77c89df510da397e
Desmond Sofua <mrsofua@gmail.com>
2023-11-09 03:38:34 +0000

    Initial Commit

---
+++ opera/operaApp.swift
@@ -0,0 +1,17 @@
+//
+//  operaApp.swift
+//  opera
+//
+//  Created by Desmond Sofua on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct operaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
